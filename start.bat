6fa727f3cd6f67c16a1d53878f105346405c9878
JCaasi74147 <caasi.jordan@ufl.edu>
2023-12-03 17:34:39 -0500

    added unzipping mechanism and start.sh

--- start.bat
+++ start.bat
@@ -1,4 +1,19 @@
 @echo off
+echo Checking and unzipping data file...
+
+if not exist actor-movie-data.csv (
+    if exist actor-movie-data.zip (
+        tar -xf actor-movie-data.zip
+        echo Data file unzipped successfully.
+    ) else (
+        echo actor-movie-data.zip not found. Please ensure it is in the current directory.
+        pause
+        exit /b
+    )
+) else (
+    echo Data file already exists.
+)
+
 echo Compiling C++ code...
 g++ main.cpp -o main.exe
 if %errorlevel% neq 0 pause
---
+++ start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Checking and unzipping data file..."
+
+if [[ ! -f actor-movie-data.csv ]]; then
+    if [[ -f actor-movie-data.zip ]]; then
+        unzip actor-movie-data.zip
+        echo "Data file unzipped successfully."
+    else
+        echo "actor-movie-data.zip not found. Please ensure it is in the current directory."
+        exit 1
+    fi
+else
+    echo "Data file already exists."
+fi
+
+echo "Compiling C++ code..."
+g++ main.cpp -o main
+if [[ $? -ne 0 ]]; then
+    echo "Compilation failed."
+    exit 1
+fi
+
+echo "Starting Flask app..."
+python app.py &
+
+echo "Opening browser..."
+open http://127.0.0.1:5000
+
+echo "Done."
